dcb127afb67b8f67ed9a8d7e7124f3608dad5efe
hicks27 <hicksp2597@student.faytechcc.edu>
2017-07-14 13:49:33 -0700

    Add files via upload
    M6HW1 completed

---
+++ M6HW1_RandomNumberFileWriter_PatriciaHicks.py
@@ -0,0 +1,10 @@
+# Random Number file writer.
+# 7/14/17
+# CTI-110 M6HW1 - Random Number File Writer
+# Patricia Hicks
+#
+import random
+
+#Random Number from 1 to 100
+x = random.randint(1, 100)
+print( x )
